c067f467bf889cfd2b8e20d2cba7bf318a2669a4
Viet Le <quocvietlex@gmail.com>
2026-06-18 14:48:09 +0300

    add tables from static files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import zipfile
 import requests
 import psycopg2
+import os
 from tqdm import tqdm
 from pathlib import Path
 
@@ -40,13 +41,126 @@ def get_gtfs_data():
     else:
         print(f"Dir already exists at {dir_output_path}")
 
+def get_cols(file_path):
+
+    type_map = {
+        "agency_id" : "TEXT",
+        "agency_name" : "TEXT",
+        "agency_url" : "TEXT",
+        "agency_timezone" : "TEXT",
+        "agency_lang" : "VARCHAR(2)",
+        "agency_phone" : "TEXT",
+        "agency_fare_url" : "TEXT",
+        "service_id" : "TEXT",
+        "monday" : "INTEGER",
+        "tuesday" : "INTEGER",
+        "wednesday" : "INTEGER",
+        "thursday" : "INTEGER",
+        "friday" : "INTEGER",
+        "saturday" : "INTEGER",
+        "sunday" : "INTEGER",
+        "start_date" : "DATE",
+        "end_date" : "DATE",
+        "exception_type" : "INTEGER",
+        "id" : "INTEGER",
+        "name" : "TEXT",
+        "route_id" : "TEXT",
+        "route_short_time" : "INTEGER",
+        "route_long_name" : "TEXT",
+        "route_type" : "INTEGER",
+        "route_url" : "TEXT",
+        "route_color" : "TEXT",
+        "route_text_color" : "TEXT",
+        "shape_id" : "TEXT",
+        "shape_pt_lat" : "FLOAT",
+        "shape_pt_lon" : "FLOAT",
+        "shape_pt_sequence" : "INTEGER",
+        "stop_id" : "TEXT",
+        "stop_code" : "TEXT",
+        "stop_name" : "TEXT",
+        "stop_lat" : "FLOAT",
+        "stop_lon" : "FLOAT",
+        "zone_id" : "CHAR(1)",
+        "wheelchair_boarding" : "TEXT",
+        "wheelchair_accessible" : "TEXT",
+        "municipality_id" : "INTEGER",
+        "from_stop_id" : "TEXT",
+        "to_stop_id" : "TEXT",
+        "transfer_type" : "INTEGER",
+        "min_transfer_time" : "TEXT",
+        "from_route_id" : "TEXT",
+        "from_trip_id" : "TEXT",
+        "to_route_id" : "TEXT",
+        "to_trip_id" : "TEXT",
+        "trip_id" : "TEXT",
+        "trip_headsign" : "TEXT",
+        "direction_id" : "TEXT",
+        "block_id" : "TEXT",
+        "shape_id" : "TEXT",
+        "bikes_allowed" : "INTEGER",
+        "date" : "DATE",
+        "route_short_name" : "TEXT"
+    }
+
+    with open(file_path) as f:
+        header = f.readline().strip().split(',')
+        cols = ", ".join([f"{c} {type_map[c]}" for c in header])
+        return cols
+        
+def create_table(cur, table_name, file_path):
+
+        cols = get_cols(file_path)
+
+        cur.execute(
+            f"""
+                CREATE TABLE IF NOT EXISTS {table_name}  (
+                {cols}
+                )
+            """
+        )
+
+def copy_table(cur, table_name, file_path):
+
+    cur.execute(f"SELECT EXISTS (SELECT 1 FROM {table_name})")
+    not_empty = cur.fetchone()[0]
+
+    if (not_empty):
+        print("THIS SHIT IS NOT EMPTY")
+        return
+
+
+    cols = get_cols(file_path)
+    with open(file_path, 'r') as f:
+        cur.copy_expert(f"COPY {table_name} FROM STDIN CSV HEADER", f)
+
+def get_files():
+    dir = "data/extended_gtfs_tampere"
+    txt_files = os.listdir(dir)
+    abs_paths = [os.path.abspath(os.path.join(dir, f)) for f in txt_files]
+    return abs_paths
+        
+
+def initialize_db(cur):
+
+    abs_paths = get_files()
+    for file in abs_paths:
+        if (Path(file).stem not in ["stop_times", "fare_attributes", "fare_rules"]):
+            print(f"creating table for {file}")
+            table_name = file.split('/')[-1].split('.')[0]
+            create_table(cur, table_name, file)
+            copy_table(cur, table_name, file)
+
 def main():
     get_gtfs_data()
 
+
     conn = psycopg2.connect(dbname="gtfs_warehouse")
     cur = conn.cursor()
-    cur.execute("SELECT 1 as connected")
-    print(cur.fetchone())
+
+    initialize_db(cur)
+    conn.commit()
+
+
 
 if __name__ == "__main__":
     main()
